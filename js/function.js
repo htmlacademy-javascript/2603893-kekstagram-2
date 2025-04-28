7b54e27e647456da46f7f29620a2db8202d9caad
Shokhrukh <sohruhakubov688@gmail.com>
2025-04-28 13:09:45 +0300

    добавляет функции (#4)

---
+++ js/function.js
@@ -0,0 +1,42 @@
+function checkStringLenght (string, length) {
+  if(string.length <= length) {
+    return true;
+  }
+
+  return false;
+}
+
+checkStringLenght('проверяемая строка', 20);
+checkStringLenght('проверяемая строка', 18);
+checkStringLenght('проверяемая строка', 10);
+
+function checkPallid (string) {
+  const normalizeString = string.replaceAll(' ', '').toLowerCase();
+  let newString = '';
+
+  for(let i = normalizeString.length - 1; i >= 0; i--) {
+    newString += normalizeString[i];
+  }
+
+  return newString === normalizeString;
+}
+
+checkPallid('топот');
+
+
+function getNumber(string) {
+  const newStr = string.toString();
+  let getStr = '';
+
+  for (let i = 0; i < newStr.length; i++) {
+    if (!isNaN(newStr[i]) && newStr[i] !== ' ') {
+      getStr += newStr[i];
+    }
+  }
+
+  return getStr.length > 0 ? getStr : NaN;
+}
+
+
+getNumber('2023');
+
