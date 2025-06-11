51de9f34c6b9089a35b1de820a7611886a4e162e
Shokhrukh <sohruhakubov688@gmail.com>
2025-06-11 13:34:03 +0300

    Модуляция (#9)

--- js/comments/init-comments.js
+++ js/comments/init-comments.js
@@ -1,22 +1,22 @@
-import { NAMES, MESSAGE } from "../constants";
-import { getRandomInteger, getRandomArrayElement } from "../utils";
+import { NAMES, MESSAGE } from '../constants';
+import { getRandomInteger, getRandomArrayElement } from '../utils';
 
 let id = 0;
 
 const initComments = () => {
-    const randomMessage = MESSAGE.split('. ');
-    const randomMessages = getRandomArrayElement(randomMessage).trim();
-    const randomName = getRandomArrayElement(NAMES);
-    const randomAvatarNumber = getRandomInteger(1, 6);
+  const randomMessage = MESSAGE.split('. ');
+  const randomMessages = getRandomArrayElement(randomMessage).trim();
+  const randomName = getRandomArrayElement(NAMES);
+  const randomAvatarNumber = getRandomInteger(1, 6);
 
-    id++;
+  id++;
 
-    return  {
-        id: id,
-        avatar: `img/avatar-${randomAvatarNumber}.svg`,
-        message: randomMessages,
-        name: randomName
-    }
-}
+  return {
+    id: id,
+    avatar: `img/avatar-${randomAvatarNumber}.svg`,
+    message: randomMessages,
+    name: randomName
+  };
+};
 
-export {initComments}
+export {initComments};
