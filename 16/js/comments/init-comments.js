64ba7aadd7c1c53c2a8d7e97d7f8f6c3dfb1532e
Keks <keks@htmlacademy.ru>
2025-06-18 20:06:10 +0300

    :heavy_check_mark: Сборка #16

---
+++ 16/js/comments/init-comments.js
@@ -0,0 +1,22 @@
+import { NAMES, MESSAGE } from '../constants';
+import { getRandomInteger, getRandomArrayElement } from '../utils';
+
+let id = 0;
+
+const initComments = () => {
+  const randomMessage = MESSAGE.split('. ');
+  const randomMessages = getRandomArrayElement(randomMessage).trim();
+  const randomName = getRandomArrayElement(NAMES);
+  const randomAvatarNumber = getRandomInteger(1, 6);
+
+  id++;
+
+  return {
+    id: id,
+    avatar: `img/avatar-${randomAvatarNumber}.svg`,
+    message: randomMessages,
+    name: randomName
+  };
+};
+
+export {initComments};
